2911d84d2f91454b12cb8c064a3a4578943c0a58
Mayurdhvajsinh Jadeja <mayurdhvajsinhjadeja123@gmail.com>
2022-11-04 15:41:38 +0530

    Add files via upload

---
+++ Day018.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int n,i,j;
+	printf("Enter Value: ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		for(j=1;j<=n;j++){
+			printf("%4d",i*j);
+		}
+		printf("\n");
+	}
+}
